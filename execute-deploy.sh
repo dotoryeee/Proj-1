97c50d6b8dc6136d76e4cc91a6ea8c6d4829b532
dotoryeee <bayer@kakao.com>
2021-03-16 12:34:50 +0900

    nginx fix

--- execute-deploy.sh
+++ execute-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #EC2 deploy 초기에 CodeDeploy 에이전트와 도커 설치를 위해 20초 대기
 sleep 20s
-#docker-compose -f /home/ec2-user/app/docker-compose-ec2.yml rm -fs
+docker-compose -f /home/ec2-user/app/docker-compose-ec2.yml rm -fs
 docker system prune -a -f
 docker-compose -f /home/ec2-user/app/docker-compose-ec2.yml up -d
